4083cd30d2056c069d891a79efe3efb5efca5d45
Kynhoff <rkynhoff@gmail.com>
2024-11-16 22:04:23 -0500

    Uploading different file names for my cipher as a result of multiple attempts at getting the cipher to work.

---
+++ cipher_4.py
@@ -0,0 +1,42 @@
+#ENCRYPTION CODE
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+new_message = ""
+
+user_message = input("Enter your secret message: ").lower()
+key = int(input("Enter a key (1 through 26): "))
+#print(user_input)
+
+for character in user_message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        new_position = (position + key) % 26
+        new_character = alphabet[new_position]
+        new_message += new_character
+
+    else:
+        new_message += character
+print("Your secret message is " + new_message)
+
+#DECRYPTION CODE
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+new_message = ""
+
+user_message = input("Enter your secret message: ").lower()
+key = int(input("Enter a key (1 through 26): "))
+#print(user_input)
+
+for character in user_message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        new_position = (position - key) % 26
+        new_character = alphabet[new_position]
+        new_message += new_character
+
+    else:
+        new_message += character
+print("Your secret message is " + new_message)
+
+# I know I can define a function to improve the
+# code and reduce the repetition, but each time I
+# tried, the code nevr ran through all the input 
+# prompts. Only the initial input prompt would work. 
